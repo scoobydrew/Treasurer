9a52169388b31101d680d9ea8aaac1653c675e24
Drew Smith <drewcsmith3@gmail.com>
2017-02-12 20:37:06 -0600

    Webpack, typescript, and react set up, initial experiments

---
+++ Treasurer/Treasurer/src/index.tsx
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface ToggleButtonProps {
+    text: string;
+}
+interface ToggleButtonState {
+    toggleOn: boolean;
+}
+
+class ToggleButton extends React.Component<ToggleButtonProps, ToggleButtonState>{
+    constructor() {
+        super();
+        this.state = {};
+        this.state.toggleOn = false;
+    }
+    render() {
+        return (
+            <a onClick={this._handleClick.bind(this)} className={this.state.toggleOn ? 'toggleOn' : 'toggleOff'} > {this.props.text} </a>);
+    }
+    _handleClick(event) {
+        this.setState({ toggleOn: !this.state.toggleOn });
+    }
+}
+interface FilterBoxProps { }
+interface FilterBoxState { }
+class FilterBox extends React.Component<FilterBoxProps, FilterBoxState> {
+    const FILTERS = [
+        { text: 'Click Me!', id: 1 },
+        { text: 'No Click Me!', id: 2 }
+    ]
+    constructor()
+    {
+        super();
+    }
+    render() {
+        return (
+            <ul>
+                {this.FILTERS.map((filter) => <li><ToggleButton text={filter.text} key={filter.id} /></li>)}
+            </ul>
+        )
+    }
+}
+
+ReactDOM.render(<FilterBox />, document.getElementById("component"));
+
---
+++ Treasurer/Treasurer/webpack.config.js
@@ -0,0 +1,26 @@
+﻿var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+    devtool: 'eval',
+    // This will be our app's entry point (webpack will look for it in the 'src' directory due to the modulesDirectory setting below). Feel free to change as desired.
+    entry: [
+        'index.tsx'
+    ],
+    // Output the bundled JS to dist/app.js
+    output: {
+        filename: 'app.js',
+        path: path.resolve('dist')
+    },
+    resolve: {
+        // Look for modules in .ts(x) files first, then .js(x)
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        // Add 'src' to our modulesDirectories, as all our app code will live in there, so Webpack should look in there for modules
+        modules: ['src', 'node_modules'],
+    },
+    module: {
+        loaders: [
+            // .ts(x) files should first pass through the Typescript loader, and then through babel
+            { test: /\.tsx?$/, loaders: ['babel-loader', 'ts-loader'] }
+        ]
+    }
+};
